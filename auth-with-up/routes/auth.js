1c7269732cdd61471de5ecea838edb4275ff26de
Christian Fei <crifei93@gmail.com>
2017-10-21 17:22:35 +0200

    improve logging and set reidect routes

--- auth-with-up/routes/auth.js
+++ auth-with-up/routes/auth.js
@@ -1,59 +1,53 @@
 const router = require('express').Router()
 const passport = require('passport')
-const defaultRedirectRoutes = {failureRedirect: '/info', successRedirect: '/info'}
+const developmentRedirectRoutes = {failureRedirect: 'dev.pomodoro.cc', successRedirect: 'dev.pomodoro.cc'}
+const productionRedirectRoutes = {failureRedirect: 'pomodoro.cc', successRedirect: 'pomodoro.cc'}
+const redirectRoutes = (process.env.UP_STAGE === 'development' || process.env.NODE_ENV === 'development') ? developmentRedirectRoutes : productionRedirectRoutes
 const User = require('../models/User')
 
 router.get('/twitter', passport.authenticate('twitter'))
 router.get('/github', passport.authenticate('github'))
 
 router.get('/twitter/callback',
-  passport.authenticate('twitter', defaultRedirectRoutes))
+  passport.authenticate('twitter', redirectRoutes))
 router.get('/github/callback',
-  passport.authenticate('github', defaultRedirectRoutes))
+  passport.authenticate('github', redirectRoutes))
 
-/**
-@api {get} /info user information
-@apiGroup Auth
-@apiDescription Get informations about the user
-
-@apiHeader {String} Authorization Users unique api-key.
-@apiHeader {String} Cookie Users browser cookie.
-
-@apiSuccess {String} apikey users unique apikey
-@apiSuccess {Int} id users id
-@apiSuccess {String} avatar users avatar
-@apiSuccess {String} username users username
-@apiSuccessExample {json} Success-Response:
-    HTTP/1.1 200 OK
-    {
-      "apikey": "f4k34p1k3y",
-      "id": 123456,
-      "avatar": "https://avatars.githubusercontent.com/u/2662706?v=3",
-      "username": "rambo"
-    }
-*/
 router.get('/info', function (req, res) {
+  console.info('req.headers', req.headers)
+  console.info('redirectRoutes', redirectRoutes)
+  console.log('process.env.NODE_ENV, process.env.UP_STAGE', process.env.NODE_ENV, process.env.UP_STAGE)
   if (req.user) {
     res.json(req.user)
     return res.end()
   }
+
   var authorizationHeader = req.get('Authorization')
-  if (!authorizationHeader) {
-    return unauthorized(res)
-  }
+  if (!authorizationHeader) return unauthorized(res)
+
   var apikey = authorizationHeader.match(/token (.*)/)
   apikey = apikey ? apikey[1] : apikey
-  if (apikey) {
-    User.findOne({apikey: apikey}, function (err, user) {
-      if (err) {
-        return unauthorized(res)
-      }
-      res.json(user)
-      return res.end()
-    })
-  } else {
-    return unauthorized(res)
+
+  if (!apikey) return unauthorized(res)
+
+  User.findOne({apikey: apikey}, function (err, user) {
+    if (err) {
+      return unauthorized(res)
+    }
+    res.json(user)
+    return res.end()
+  })
+  /*
+  HTTP/1.1 200 OK
+  {
+    "_id":"f4k31df4k31d",
+    "apikey": "f4k34p1k3y",
+    "id": 123456,
+    "avatar": "https://avatars.githubusercontent.com/u/2662706?v=3",
+    "username": "christian-fei",
+    "__v":0
   }
+  */
 })
 
 function unauthorized (res) {
