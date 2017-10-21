37671acb9a901e144df1796e29e5c4c4287a5118
Christian Fei <crifei93@gmail.com>
2017-10-22 01:46:21 +0200

    remove payments

--- payments/create-subscription/test/_stripe-utils.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').load()
-
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
-
-module.exports.deleteSubscription = function deleteSubscription (id) {
-  return stripe.subscriptions.del(id)
-}
