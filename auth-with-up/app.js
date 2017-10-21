8c2d73358000140f023e98f0783f9ffa243326d0
Christian Fei <crifei93@gmail.com>
2017-10-21 14:37:30 +0200

    update origins cors

--- auth-with-up/app.js
+++ auth-with-up/app.js
@@ -15,7 +15,7 @@ console.log('initialized auth')
 
 server.use(morgan(':status\t :method\t :response-time ms\t :date[clf]\t :url\t\t'))
 server.use(cors({
-  origin: ['https://pomodoro.cc', 'http://beta.pomodoro.cc', 'https://beta.pomodoro.cc', 'https://app.pomodoro.cc', 'http://localhost:9000'],
+  origin: ['https://pomodoro.cc', 'http://beta.pomodoro.cc', 'https://beta.pomodoro.cc', 'https://app.pomodoro.cc', 'https://dev.pomodoro.cc', 'http://dev.pomodoro.cc'],
   methods: ['HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['cookie', 'with_credentials'],
   credentials: true
