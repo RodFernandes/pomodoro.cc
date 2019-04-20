46e0d51d03045eba336db3bdd1b36627d2369de9
Christian Fei <crifei93@gmail.com>
2019-04-20 20:28:27 +0200

    ismpler

--- auth/routes/healthcheck.js
+++ auth/routes/healthcheck.js
@@ -1,6 +1,9 @@
 let router = require('express').Router()
 if (!process.env.NOW && !process.env.now) {
+  console.log('in now environment')
   router = require('..')
+} else {
+  console.log('not in now environment')
 }
 
 router.get('/', function (req, res) {
