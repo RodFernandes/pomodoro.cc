d44b29ea28242458d6814735cf26b6548816a1ef
Christian Fei <crifei93@gmail.com>
2019-04-20 20:36:37 +0200

    now canary + auth-twitter

---
+++ auth/routes/auth-twitter.js
@@ -0,0 +1,21 @@
+let router = require('express').Router()
+if (!process.env.NOW && !process.env.now) {
+  console.log('in now environment')
+  router = require('..')
+} else {
+  console.log('not in now environment')
+}
+const passport = require('passport')
+const developmentRedirectRoutes = { failureRedirect: 'https://dev.pomodoro.cc', successRedirect: 'https://dev.pomodoro.cc' }
+const productionRedirectRoutes = { failureRedirect: 'https://pomodoro.cc', successRedirect: 'https://pomodoro.cc' }
+const redirectRoutes = (process.env.UP_STAGE === 'development' || process.env.NODE_ENV === 'development') ? developmentRedirectRoutes : productionRedirectRoutes
+
+router.get('/twitter', passport.authenticate('twitter'))
+router.get('/github', passport.authenticate('github'))
+
+router.get('/twitter/callback',
+  passport.authenticate('twitter', redirectRoutes))
+router.get('/github/callback',
+  passport.authenticate('github', redirectRoutes))
+
+module.exports = router
