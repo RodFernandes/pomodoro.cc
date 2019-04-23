956549312692c3615b80c8032df3c08609292a9a
Christian Fei <crifei93@gmail.com>
2019-04-23 21:02:19 +0200

    rename collection to users

--- auth/models/User.js
+++ auth/models/User.js
@@ -1,10 +1,3 @@
 const db = require('../init/mongo')()
 
-// const UserSchema = new mongoose.Schema({
-//   apikey: {type: String, required: true},
-//   id: {type: Number, required: true},
-//   avatar: {type: String, required: true},
-//   username: {type: String, required: true}
-// }, {collection: 'users'})
-
-module.exports = db.get('User')
+module.exports = db.get('users')
