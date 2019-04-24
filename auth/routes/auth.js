50c27585ad6d65fc13a72e8daf58665f0283b056
Christian Fei <crifei93@gmail.com>
2019-04-24 14:51:47 +0200

    various fixes

--- auth/routes/auth.js
+++ auth/routes/auth.js
@@ -1,70 +1,91 @@
-let router = require('express').Router()
-// if (process.env.NOW_REGION) {
-//   console.log('in now environment')
-//   router = require('..')
-// } else {
-//   console.log('not in now environment')
-// }
+const app = require('../app')
 const passport = require('passport')
-const developmentRedirectRoutes = { failureRedirect: 'https://dev.pomodoro.cc', successRedirect: 'https://dev.pomodoro.cc' }
-const productionRedirectRoutes = { failureRedirect: 'https://pomodoro.cc', successRedirect: 'https://pomodoro.cc' }
-const redirectRoutes = (process.env.UP_STAGE === 'development' || process.env.NODE_ENV === 'development') ? developmentRedirectRoutes : productionRedirectRoutes
+const session = require('express-session')
+const TwitterStrategy = require('passport-twitter').Strategy
+const MongoStore = require('connect-mongo')(session)
+const UserInfo = require('../modules/UserInfo')
 const User = require('../models/User')
 
-router.get('/twitter', passport.authenticate('twitter'))
-router.get('/github', passport.authenticate('github'))
-
-router.get('/twitter/callback',
-  passport.authenticate('twitter', redirectRoutes))
-router.get('/github/callback',
-  passport.authenticate('github', redirectRoutes))
-
-router.get('/info', function (req, res) {
-  console.info('req.headers', req.headers)
-  console.info('redirectRoutes', redirectRoutes)
-  console.log('process.env.NODE_ENV, process.env.UP_STAGE', process.env.NODE_ENV, process.env.UP_STAGE)
-  if (req.user) {
-    res.json(req.user)
-    return res.end()
-  }
-
-  var authorizationHeader = req.get('Authorization')
-  if (!authorizationHeader) return unauthorized(res)
-
-  var apikey = authorizationHeader.match(/token (.*)/)
-  apikey = apikey ? apikey[1] : apikey
-
-  if (!apikey) return unauthorized(res)
-
-  User.findOne({ apikey: apikey }, function (err, user) {
-    if (err) {
-      return unauthorized(res)
-    }
-    res.json(user)
-    return res.end()
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'foo',
+  cookie: {
+    domain: '.pomodoro.cc',
+    sameSite: false
+  },
+  store: new MongoStore({
+    collection: 'sessions',
+    url: process.env.MONGO_URL
   })
-  /*
-  HTTP/1.1 200 OK
-  {
-    "_id":"f4k31df4k31d",
-    "apikey": "f4k34p1k3y",
-    "id": 123456,
-    "avatar": "https://avatars.githubusercontent.com/u/2662706?v=3",
-    "username": "christian-fei",
-    "__v":0
-  }
-  */
-})
+}))
 
-function unauthorized (res) {
-  res.writeHead(401)
-  return res.end()
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new TwitterStrategy({
+  consumerKey: process.env.TWITTER_CONSUMER_KEY,
+  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  callbackURL: process.env.TWITTER_CALLBACK_URL
+}, authenticatedUser))
+
+function authenticatedUser (token, tokenSecret, profile, done) {
+  var user = new UserInfo(profile).toJSON()
+
+  User.findOne({ id: user.id })
+    .then(user => {
+      if (user) return done(null, user)
+      User.insert(new UserInfo(profile))
+        .then(user => {
+          done(null, user)
+        })
+        .catch(err => {
+          if (err) return done(err, null)
+        })
+    })
 }
 
-router.get('/logout', function (req, res) {
-  req.logout()
-  req.session.destroy()
-  res.redirect('/')
+passport.serializeUser(function (user, done) {
+  done(null, user)
 })
 
-module.exports = router
+passport.deserializeUser(function (user, done) {
+  done(null, user)
+})
+
+// app.get('*', passport.authenticate('twitter'), (req, res) => {
+//   delete req.session.passport
+//   console.log('twitter user', req.user)
+//   req.session.user = Object.assign({}, req.user)
+//   res.redirect('https://pomodoro.cc')
+// })
+
+const redirectRoutes = { failureRedirect: 'https://pomodoro.cc', successRedirect: 'https://pomodoro.cc' }
+
+app.get('/info', (req, res) => {
+  console.log('req.user', req.user)
+  console.log('req.session', req.session)
+  req.send(req.user)
+  // res.redirect('https://pomodoro.cc')
+})
+app.get('/twitter', passport.authenticate('twitter'))
+app.get('/twitter/callback', passport.authenticate('twitter', redirectRoutes))
+app.get('/github', passport.authenticate('github'))
+app.get('/github/callback', passport.authenticate('github', redirectRoutes))
+
+module.exports = app
+
+// const passport = require('passport')
+// const developmentRedirectRoutes = { failureRedirect: 'https://dev.pomodoro.cc', successRedirect: 'https://dev.pomodoro.cc' }
+// const productionRedirectRoutes = { failureRedirect: 'https://pomodoro.cc', successRedirect: 'https://pomodoro.cc' }
+// const redirectRoutes = (process.env.UP_STAGE === 'development' || process.env.NODE_ENV === 'development') ? developmentRedirectRoutes : productionRedirectRoutes
+
+// router.get('/twitter', passport.authenticate('twitter'))
+// router.get('/github', passport.authenticate('github'))
+
+// router.get('/twitter/callback',
+//   passport.authenticate('twitter', redirectRoutes))
+// router.get('/github/callback',
+//   passport.authenticate('github', redirectRoutes))
+
+// module.exports = router
