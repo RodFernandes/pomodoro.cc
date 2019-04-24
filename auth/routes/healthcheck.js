1dd5d5ec29d6fa67f4389207d3e5574c05ee53ec
Christian Fei <crifei93@gmail.com>
2019-04-24 10:42:07 +0200

    setup simpler healthcheck

--- auth/routes/healthcheck.js
+++ auth/routes/healthcheck.js
@@ -1,13 +1,3 @@
-let router = require('express').Router()
-if (process.env.NOW_REGION) {
-  console.log('in now environment')
-  router = require('..')
-} else {
-  console.log('not in now environment')
+module.exports = (req, res) => {
+  res.end('ok')
 }
-
-router.get('/', function (req, res) {
-  res.end()
-})
-
-module.exports = router
