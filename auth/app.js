b578e37587d8509e9b4ccf5f2343916a0f3e2e51
Christian Fei <crifei93@gmail.com>
2019-05-01 17:02:11 +0200

    no log MONGO_URL when starting and env

--- auth/app.js
+++ auth/app.js
@@ -2,14 +2,13 @@ if (!process.env.NOW_REGION) { require('dotenv').config() }
 const app = require('express')()
 const middlewares = require('./middlewares')
 
-console.log('env', process.env)
 if (!process.env.NOW_REGION) {
   const { PORT = 3000 } = process.env
   app.listen(PORT)
   console.log(`listening @ https://auth.pomodoro.cc (PORT ${PORT})`)
 }
 
-console.log('MONGO_URL', process.env.MONGO_URL)
+console.log('MONGO_URL set?', !!process.env.MONGO_URL)
 
 app.set('trust proxy', 1)
 app.use(...middlewares)
