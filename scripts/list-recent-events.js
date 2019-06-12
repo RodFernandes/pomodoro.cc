44c0d17067a19301340c4625b2dd8001cc80affc
Christian Fei <crifei93@gmail.com>
2019-06-12 20:14:10 +0200

    improve list-recent-events

--- scripts/list-recent-events.js
+++ scripts/list-recent-events.js
@@ -5,7 +5,7 @@ const chalk = require('chalk')
 const stringToColor = require('string-to-color')
 
 if (require.main === module) {
-  main(process.argv[2])
+  main({ _id: process.argv[2] })
     .then(res => {
       console.log(res)
       process.exit(0)
@@ -18,33 +18,42 @@ if (require.main === module) {
 
 module.exports = main
 
-async function main (param) {
-  if (param) {
-    return Event.find({ _id: param }).then(console.log)
+async function main ({ _id }) {
+  if (_id) {
+    return Event.findOne({ _id: _id }).then(printEvent)
   }
 
   const events = await Event.find({}, { limit: 250, sort: { createdAt: -1 } })
   events
     .sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
-    .forEach(e => {
-      const createdAt = e.createdAt
-      const username = e.user && e.user.username
-      const eventName = e.name
-      let additionalInfo = ''
+    .forEach(printEvent)
+}
 
-      if (e.name === 'createUserSucceeded') {
-        additionalInfo = e.user && e.user.username
-      }
-      if (e.name === 'userAuthenticated') {
-        additionalInfo = e.user && e.user.username
-      }
-      if (e.name === 'createPomodoro') {
-        additionalInfo = e.user && e.user.username
-      }
-      if (e.name === 'pomodoroFailedValidation') {
-        additionalInfo = `${e.user && e.user.username}\n\terrors: ${(e.errors || []).join(', ')}\n\tpomodoro: ${JSON.stringify(e.pomodoro || {})}`
-      }
+function printEvent (e) {
+  console.log(eventToString(e))
+}
 
-      console.log(`${chalk.blue(createdAt)} ${chalk.white(e._id)} ${chalk.hex(stringToColor(eventName)).bold(eventName)} by user ${chalk.yellow(username)} ${additionalInfo}`)
-    })
+function eventToString (e) {
+  const createdAt = e.createdAt
+  const username = e.user && e.user.username
+  const eventName = e.name
+  const additionalInfo = additionalInfoFor(e)
+
+  return `${chalk.blue(createdAt)} ${chalk.white(e._id)} ${chalk.hex(stringToColor(eventName)).bold(eventName)} by user ${chalk.yellow(username)} ${additionalInfo}`
+}
+
+function additionalInfoFor (e) {
+  if (e.name === 'createUserSucceeded') {
+    return e.user && e.user.username
+  }
+  if (e.name === 'userAuthenticated') {
+    return e.user && e.user.username
+  }
+  if (e.name === 'createPomodoro') {
+    return e.user && e.user.username
+  }
+  if (e.name === 'pomodoroFailedValidation') {
+    return `${e.user && e.user.username}\n\terrors: ${(e.errors || []).join(', ')}\n\tpomodoro: ${JSON.stringify(e.pomodoro || {})}`
+  }
+  return ''
 }
